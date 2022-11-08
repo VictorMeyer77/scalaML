d9e2ac201bb14b1428c738038fba7ffb76415c9d
Victor-Algo <victor.algo.contact@gmail.com>
2022-11-08 20:56:41 +0100

    add logging trait

---
+++ src/main/scala/com/victormeyer/scalaml/Logging.scala
@@ -0,0 +1,12 @@
+package com.victormeyer.scalaml
+
+import org.apache.log4j.Logger
+
+/** Trait to implement to log with log4j
+ *
+ */
+trait Logging {
+
+  val logger: Logger = Logger.getLogger(this.getClass)
+
+}
