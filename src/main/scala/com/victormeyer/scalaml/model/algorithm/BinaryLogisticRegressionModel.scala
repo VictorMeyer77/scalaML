9a5e07ba6ce5aab33086856d67f742a345223298
Victor-Algo <victor.algo.contact@gmail.com>
2022-11-16 21:12:32 +0100

    add BinaryLogisticRegressionModel

---
+++ src/main/scala/com/victormeyer/scalaml/model/algorithm/BinaryLogisticRegressionModel.scala
@@ -0,0 +1,21 @@
+package com.victormeyer.scalaml.model.algorithm
+
+import com.victormeyer.scalaml.model.Model
+
+/** Represent binary logistic regression model
+ *
+ * @param weights Array of weights
+ * @param bias Bias of model
+ */
+class BinaryLogisticRegressionModel(var weights: Array[Double], var bias: Double) extends Model {
+
+  /** Convert model to string
+   *
+   *  @return Json format string
+   */
+  override def toString: String = {
+    s"""{"weights": [${weights.mkString(", ")}], "bias": $bias}"""
+  }
+
+
+}
